5a108ef3f47d95c753073ce19dbe4f19ad17a70a
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 14:15:27 -0400

    Update Barbarian.cpp

--- Barbarian.cpp
+++ Barbarian.cpp
@@ -3,3 +3,30 @@
 ** Date:        8/6/2018
 ** Description: Barbarian class definition for fantasy game
 **********************************************************************/
+
+#include "Barbarian.hpp"
+
+Barbarian::Barbarian()
+{
+    atkDie = 2;
+    defDie = 2;
+    atkNum = 6;
+    defNum = 6;
+    armor = 0;
+    strength = 12;
+}
+
+Barbarian::~Barbarian()
+{
+    
+}
+
+int Barbarian::attack()
+{
+    
+}
+
+int Barbarian::defense()
+{
+    
+}
