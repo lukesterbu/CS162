dc942b6aff8838bac4f5b23caa3722400698b953
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 11:38:58 -0400

    Create fantasyMenu.hpp

---
+++ fantasyMenu.hpp
@@ -0,0 +1,12 @@
+/**********************************************************************
+** Author:      Luke Burris
+** Date:        8/6/2018
+** Description: Menu function prototype for fantasy game
+**********************************************************************/
+
+#ifndef FANTASYMENU_HPP
+#define FANTASYMENU_HPP
+
+void fantasyMenu(); // function prototype
+
+#endif
