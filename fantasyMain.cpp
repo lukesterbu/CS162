70b618bbcfb10b86a7be2c43a495590773564521
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 19:47:37 -0400

    Update fantasyMain.cpp

--- fantasyMain.cpp
+++ fantasyMain.cpp
@@ -1,1 +1,14 @@
+/**********************************************************************
+** Author:      Luke Burris
+** Date:        8/6/2018
+** Description: Barbarian class definition for fantasy game
+**********************************************************************/
 
+#include "fantasyMenu.hpp"
+
+int main()
+{
+	fantasyMenu();
+	
+	return 0;  
+}
