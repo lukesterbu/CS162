44dfe8ef34ce3f22551af597aea03e5716a96331
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 14:21:41 -0400

    Update Vampire.cpp

--- Vampire.cpp
+++ Vampire.cpp
@@ -3,3 +3,30 @@
 ** Date:        8/6/2018
 ** Description: Vampire class definition for fantasy game
 **********************************************************************/
+
+#include "Vampire.hpp"
+
+Vampire::Vampire()
+{
+    atkDie = 1;
+    defDie = 1;
+    atkNum = 12;
+    defNum = 6;
+    armor = 1;
+    strength = 18;
+}
+
+Vampire::~Vampire()
+{
+    
+}
+
+int Vampire::attack()
+{
+    
+}
+
+int Vampire::defense()
+{
+    
+}
