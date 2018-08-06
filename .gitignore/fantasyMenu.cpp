ae329bdda7f05400f1a3f6770f0f23f75ce57822
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 11:32:30 -0400

    Update fantasyMenu.cpp

--- .gitignore/fantasyMenu.cpp
+++ .gitignore/fantasyMenu.cpp
@@ -1,7 +1,52 @@
 #include "fantasyMenu.hpp"
 
+/**********************************************************************
+** Author:      Luke Burris
+** Date:        8/6/2018
+** Description: Menu function for Project 3
+**********************************************************************/
+
+#include "fantasyMenu.hpp"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 void fantasyMenu()
 {
-
-
+    int choice = 0;
+    
+    for (int i = 1; i < 3; i++) // iterate twice for 2 players
+    {
+        cout << "PLAYER " << i << endl;
+        cout << "Choose a character:" << endl; 
+        cout << "1. Vampire" << endl;
+        cout << "2. Barbarian" << endl;
+        cout << "3. Blue Men" << endl;
+        cout << "4. Medusa" << endl;
+        cout << "5. Harry Potter" << endl;
+        cin >> choice; // will use inputValidation here
+        
+        if (choice == 1) // Create Vampire
+        {
+            
+        }
+        else if (choice == 2) // Create Barbarian
+        {
+            
+        }
+        else if (choice == 3) // Create Blue Men
+        {
+            
+        }
+        else if (choice == 4) // Create Medusa
+        {
+            
+        }
+        else if (choice == 5) // Create Harry Potter
+        {
+            
+        }
+    }
 }
