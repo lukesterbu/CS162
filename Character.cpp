2fe8b297636fe49d4fdbdea3505897b2092c859a
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 14:42:00 -0400

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -1,53 +1,33 @@
 /**********************************************************************
 ** Author:      Luke Burris
 ** Date:        8/6/2018
-** Description: Menu function for fantasy game
+** Description: Character class definition for fantasy game
 **********************************************************************/
 
-#include "fantasyMenu.hpp"
 #include "Character.hpp"
 
-#include <iostream>
-
-using std::cout;
-using std::endl;
-using std::cin;
-
-void fantasyMenu()
+Character::Character()
 {
-    int choice = 0;
-    Character playerArray[2];
-    
-    for (int i = 0; i < 2; i++) // iterate twice for 2 players
-    {
-        cout << "PLAYER " << i + 1 << endl;
-        cout << "Choose a character:" << endl; 
-        cout << "1. Vampire" << endl;
-        cout << "2. Barbarian" << endl;
-        cout << "3. Blue Men" << endl;
-        cout << "4. Medusa" << endl;
-        cout << "5. Harry Potter" << endl;
-        cin >> choice; // will use inputValidation here
-        
-        if (choice == 1) // Create Vampire
-        {
-            playerArray[i] = new Vampire;
-        }
-        else if (choice == 2) // Create Barbarian
-        {
-            playerArray[i] = new Barbarian;
-        }
-        else if (choice == 3) // Create Blue Men
-        {
-            playerArray[i] = new BlueMen;
-        }
-        else if (choice == 4) // Create Medusa
-        {
-            playerArray[i] = new Medusa;
-        }
-        else if (choice == 5) // Create Harry Potter
-        {
-            playerArray[i] = new HarryPotter;
-        }
-    }
+    atkDie = 0;
+    defDie = 0;
+    atkNum = 0;
+    defNum = 0;
+    armor = 0;
+    strength = 0;
 }
+
+Character::~Character()
+{}
+
+int Character::attack()
+{}
+
+int Character::defense()
+{}
+
+void Character::printStats()
+{
+    //Attacker type.
+    //Defender type, armor, strength point.
+    //The attacker's attack dice roll.
+    //The defenderb
