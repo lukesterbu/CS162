3017d8e56b2a5022801f3502fbe77a4c8c542775
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 14:02:51 -0400

    Update fantasyMenu.cpp

--- fantasyMenu.cpp
+++ fantasyMenu.cpp
@@ -1,25 +1,33 @@
-#include "fantasyMenu.hpp"
-
 /**********************************************************************
 ** Author:      Luke Burris
 ** Date:        8/6/2018
 ** Description: Menu function for fantasy game
 **********************************************************************/
 
-#include "fantasyMenu.hpp"
+#include "Character.hpp"
+#include "Vampire.hpp"
+#include "Barbarian.hpp"
+#include "BlueMen.hpp"
+#include "Medusa.hpp"
+#include "HarryPotter.hpp"
 
 #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::endl;
+using std::cin;
+using std::vector;
 
 void fantasyMenu()
 {
+    int numPlayers = 2;
     int choice = 0;
+    vector<Character*> characterArray;
     
-    for (int i = 1; i < 3; i++) // iterate twice for 2 players
+    for (int i = 0; i < numPlayers; i++) // iterate twice for 2 players
     {
-        cout << "PLAYER " << i << endl;
+        cout << "PLAYER " << i + 1 << endl;
         cout << "Choose a character:" << endl; 
         cout << "1. Vampire" << endl;
         cout << "2. Barbarian" << endl;
@@ -30,23 +38,23 @@ void fantasyMenu()
         
         if (choice == 1) // Create Vampire
         {
-            
+            characterArray.push_back(new Vampire());
         }
         else if (choice == 2) // Create Barbarian
         {
-            
+            characterArray.push_back(new Barbarian());
         }
         else if (choice == 3) // Create Blue Men
         {
-            
+            characterArray.push_back(new BlueMen());
         }
         else if (choice == 4) // Create Medusa
         {
-            
+            characterArray.push_back(new Medusa());
         }
         else if (choice == 5) // Create Harry Potter
         {
-            
+            characterArray.push_back(new HarryPotter());
         }
     }
 }
