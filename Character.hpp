0a6ba5e004c8e24ca8bdeeb895a47bb9bcd9bf81
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 11:52:55 -0400

    Update Character.hpp

--- Character.hpp
+++ Character.hpp
@@ -10,6 +10,15 @@
 class CHARACTER_HPP
 {
     protected:
-        
+        int atkDie; // how many attack dies
+        int defDie; // how many defense dies
+        int atkNum; // how many attack die faces
+        int defNum; // how many defense die faces
+        int armor; 
+        int strength; // hit points
     public:
+        Character();
+        ~Character();
+        virtual int attack();
+        virtual int defense();
 }
