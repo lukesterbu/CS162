27186850b80a04a4b9b4519690b06dc44ef08ddf
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 11:39:31 -0400

    Create Character.hpp

---
+++ Character.hpp
@@ -0,0 +1,15 @@
+/**********************************************************************
+** Author:      Luke Burris
+** Date:        8/6/2018
+** Description: Character declaration for fantasy game
+**********************************************************************/
+
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+class CHARACTER_HPP
+{
+    protected:
+        
+    public:
+}
