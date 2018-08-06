664bde5953ce34b075eae2814a42588671aeb5f7
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 14:05:42 -0400

    Create HarryPotter.hpp

---
+++ HarryPotter.hpp
@@ -0,0 +1,21 @@
+/**********************************************************************
+** Author:      Luke Burris
+** Date:        8/6/2018
+** Description: Harry Potter class declaration for fantasy game
+**********************************************************************/
+
+#ifndef HARRYPOTTER_HPP
+#define HARRYPOTTER_HPP
+
+#include "Character.hpp"
+
+class HarryPotter : public Character
+{
+    private:
+        int lives; // only Harry needs this because of hogwarts ability
+    public:
+        int attack();
+        int defense();
+};
+
+#endif
