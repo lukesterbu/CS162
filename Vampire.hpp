936065e6ededf63974bd896e88ae886b8a975113
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 12:25:38 -0400

    Create Vampire.hpp

---
+++ Vampire.hpp
@@ -0,0 +1,19 @@
+/**********************************************************************
+** Author:      Luke Burris
+** Date:        8/6/2018
+** Description: Vampire class declaration for fantasy game
+**********************************************************************/
+
+#ifndef VAMPIRE_HPP
+#define VAMPIRE_HPP
+
+#include "Character.hpp"
+
+class Vampire
+{
+    public:
+        int attack();
+        int defense();
+};
+
+#endif
