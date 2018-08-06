dfe3422ede5ea9d628aef577302885e184e476c3
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 14:15:48 -0400

    Update HarryPotter.cpp

--- HarryPotter.cpp
+++ HarryPotter.cpp
@@ -3,3 +3,31 @@
 ** Date:        8/6/2018
 ** Description: Harry Potter class definition for fantasy game
 **********************************************************************/
+
+#include "HarryPotter.hpp"
+
+HarryPotter::HarryPotter()
+{
+    atkDie = 2;
+    defDie = 2;
+    atkNum = 6;
+    defNum = 6;
+    armor = 0;
+    strength = 10;
+    lives = 1;
+}
+
+HarryPotter::~HarryPotter()
+{
+    
+}
+
+int HarryPotter::attack()
+{
+    
+}
+
+int HarryPotter::defense()
+{
+    
+}
