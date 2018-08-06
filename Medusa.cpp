2fceff0dbebc906c5e8159c2d26c7629cbc2d442
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-06 14:21:21 -0400

    Update Medusa.cpp

--- Medusa.cpp
+++ Medusa.cpp
@@ -3,3 +3,30 @@
 ** Date:        8/6/2018
 ** Description: Medusa class definition for fantasy game
 **********************************************************************/
+
+#include "Medusa.hpp"
+
+Medusa::Medusa()
+{
+    atkDie = 2;
+    defDie = 1;
+    atkNum = 6;
+    defNum = 6;
+    armor = 3;
+    strength = 8;
+}
+
+Medusa::~Medusa()
+{
+    
+}
+
+int Medusa::attack()
+{
+    
+}
+
+int Medusa::defense()
+{
+    
+}
