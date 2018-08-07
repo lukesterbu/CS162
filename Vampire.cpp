da0ce9eac7111b08b16c81aa1d331d2a2edf694a
Luke <41594454+lukesterbu@users.noreply.github.com>
2018-08-07 16:13:49 -0400

    Edit defense to incorporate charm

--- Vampire.cpp
+++ Vampire.cpp
@@ -27,23 +27,28 @@ Vampire::~Vampire()
     
 }
 
-int Vampire::charm(int attack)
-{
-    int charm = attack;
-    if (((rand() % 2) + 1) == 2)
-    {
-	charm = 0;
-	cout << "The Vampire used charm!" << endl;
-    }
-    return charm;
-}
-
-int Vampire::defense()
+// Runs charm and calculates damage done
+// Returns defense dice roll
+int Vampire::defense(int attack)
 {
     int defense = 0;
     for (int i = 0; i < defDie; i++)
     {
         defense += rand() % (defNum - 1 + 1) + 1;
     }
+    // Use charm
+    if (((rand() % 2) + 1) == 2)
+    {
+	attack = 0;
+	cout << "The Vampire used charm!" << endl;
+    }
+	
+    if ((defense + armor) >= attack)
+	difference = 0;
+    else
+	difference = attack - defense - armor;
+	
+    setStrength(strength - difference);
+	
     return defense;
 }
